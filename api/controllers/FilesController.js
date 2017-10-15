d506ac1d2a83d7e025564cfa1dcd2cbd77d9f1ad
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2017-10-15 23:39:25 +0800

    Create new 'files' api

---
+++ api/controllers/FilesController.js
@@ -0,0 +1,10 @@
+/**
+ * FilesController
+ *
+ * @description :: Server-side logic for managing files
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
