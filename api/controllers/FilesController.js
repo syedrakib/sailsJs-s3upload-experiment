de58b6845adb2d18d3f6414734818b1044b8e72e
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2017-10-15 23:59:41 +0800

    Create a landing page as index function

--- api/controllers/FilesController.js
+++ api/controllers/FilesController.js
@@ -5,6 +5,17 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
+function index (request, response) {
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.end(
+        '<form action="/files/upload" enctype="multipart/form-data" method="post">\n' +
+        '\t<label>Username: </label><input type="text" name="username"><br />\n' +
+        '\t<label>Avatar: </label><input type="file" name="avatar"><br />\n' +
+        '\t<input type="submit" value="Upload" />\n' +
+        '</form>'
+    );
+};
 
+module.exports = {
+    index: index,
 };
